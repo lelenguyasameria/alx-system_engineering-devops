1a844280a7ae75e406248497484853ec19210c27
lelenguyasameria <somearia711@gmail.com>
2023-11-24 08:46:14 -0800

    installation

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,14 @@
+# Puppet manifest to install Flask version 2.1.0
+# File: install_flask.pp
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
+  require => Package['python3-pip'],
+}
+
