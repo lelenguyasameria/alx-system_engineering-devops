3bd34d621e2ef4008c007abc859cfdeaf68815aa
lelenguyasameria <somearia711@gmail.com>
2023-11-24 08:43:45 -0800

    puppet file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# Puppet manifest to create a file in /tmp
+# File: create_school_file.pp
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
