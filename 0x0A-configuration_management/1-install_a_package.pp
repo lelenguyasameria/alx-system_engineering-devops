6b605924c62014c60680c70a9529f6133a9db8e8
lelenguyasameria <somearia711@gmail.com>
2023-11-24 08:48:57 -0800

    installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,7 +8,7 @@ package { 'python3-pip':
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
   require => Package['python3-pip'],
 }
 
