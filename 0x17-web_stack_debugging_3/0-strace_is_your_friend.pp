cbb0cc39090b55bd8438ea8a2c5b01834425a117
lelenguyasameria <somearia711@gmail.com>
2024-01-21 08:50:59 +0300

    apache2

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,18 @@
 # 0-strace_is_your_friend.pp
 # Puppet manifest to fix Apache 500 error identified using strace
 
-file { '/etc/apache2/httpd.conf':
-  ensure  => present,
-  content => 'Updated content based on strace findings',
-  notify  => Service['apache2'],
-  require => Package['apache2'], # Make sure Apache is installed first
-}
-
 package { 'apache2':
   ensure => installed,
 }
 
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello, this is the correct page!',
+}
+
 service { 'apache2':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
+  require => Package['apache2'],
 }
 
